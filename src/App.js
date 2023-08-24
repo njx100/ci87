07496e3eb68abfe73d1fcf98d959cd8ebc8c06ec
Dnnguyen <dnnguyen100@gmail.com>
2023-08-24 08:13:50 +0700

    update todoheadder to function correctly

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import NewTask from "./components/NewTask/NewTask";
 import Footer from "./components/Footer/Footer";
 import TasksList from "./components/TodoList/TasksList";
+import TodoHeader from "./components/Header/TodoHeader";
 
 const App = () => {
   return (
     <div className="App">
-      <NewTask />
+      <TodoHeader />
 
       <TasksList />
       <Footer />
