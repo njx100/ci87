36f28e43c5952fd7ecd88fdeca0709ab656ec738
njx100 <dnnguyen100@gmail.com>
2023-08-24 21:02:35 +0700

    update to radio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import NewTask from "./components/NewTask/NewTask";
+
 import Footer from "./components/Footer/Footer";
 import TasksList from "./components/TodoList/TasksList";
 import TodoHeader from "./components/Header/TodoHeader";
