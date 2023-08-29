4763138a0fef83079e61fdb802f55153b3083b9f
njx100 <dnnguyen100@gmail.com>
2023-08-29 16:17:02 +0700

    update hw4

--- src/Data/Todos.js
+++ src/Data/Todos.js
@@ -1,7 +1,7 @@
 export const TODOS = [
   { id: "1", task: "Clean up bedroom", isCompleted: false },
   { id: "2", task: "Buy some milk", isCompleted: false },
-  { id: "3", task: "Jogging", isCompleted: true },
+  { id: "3", task: "Jogging", isCompleted: false },
   { id: "4", task: "Learn React", isCompleted: false },
   { id: "5", task: "Doing Excercises", isCompleted: false },
 ];
