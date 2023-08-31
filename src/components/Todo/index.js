6d722e9ddd6560e2ddba1c0e4b8bbd5ea5eb4b65
njx100 <dnnguyen100@gmail.com>
2023-08-31 08:43:11 +0700

    completed hw5-1

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import "./style.css";
 
 const Todo = ({ todo, isCompleted }) => {
+  const [isDone, setIsDone] = useState(isCompleted);
+  const handleCheckbox = () => {
+    isDone ? setIsDone(false) : setIsDone(true);
+  };
+
   return (
     <div className="todo-container">
-      <input type="checkbox" checked={isCompleted} />
+      <input type="checkbox" onChange={handleCheckbox} checked={isDone} />
       <label
         style={{
-          textDecorationLine: isCompleted ? "line-through" : "none",
-          opacity: isCompleted ? ".6" : "1",
+          textDecorationLine: isDone ? "line-through" : "none",
+          opacity: isDone ? ".6" : "1",
         }}
       >
         {todo}
