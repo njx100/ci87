19ac05f07ae2a7bfdc67654c54a8dce8af5654e6
njx100 <dnnguyen100@gmail.com>
2023-09-13 12:22:43 +0700

    add sort todo feature

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,35 @@
 import Todo from "../Todo";
-
+import { useState } from "react";
 import "./style.css";
 
-const TodoList = ({ todos, updateStatus, editTodo }) => {
+const TodoList = ({ todos: todoProps, updateStatus, editTodo }) => {
+  const [todos, setTodos] = useState(todoProps);
+
+  const handleSelect = (option) => {
+    switch (option) {
+      case "all":
+        setTodos(todoProps);
+        break;
+      case "active":
+        setTodos(todoProps.filter((todo) => todo.isCompleted === false));
+        break;
+      case "completed":
+        setTodos(todoProps.filter((todo) => todo.isCompleted === true));
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="todo-list">
+      <select onChange={(event) => handleSelect(event.target.value)}>
+        <option value="all">All</option>
+        <option value="active">Active</option>
+        <option value="completed">Completed</option>
+      </select>
+
       {todos.map((todo) => (
         <Todo
           key={todo.id}
